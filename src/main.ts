01bb54408c31cd9dd527db0d3011a551f7bb45ed
lionel-rowe <lionel.rowe@gmail.com>
2024-11-10 12:56:21 +0800

    Enable switching between different search results

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import bcd from '@mdn/browser-compat-data' with { type: 'json' }
 import type { CompatStatement } from '@mdn/browser-compat-data'
 // @deno-types="https://raw.githubusercontent.com/krisk/Fuse/43eebfaa35917217848958e0ccc811ca07026737/src/index.d.ts"
-import Fuse from 'fuse.js'
+import Fuse, { type FuseResult } from 'fuse.js'
 import { Command } from '@cliffy/command'
 import { type Border, border as defaultBorder, Table } from '@cliffy/table'
-import { bgGreen, brightBlack, cyan, green, red, rgb24, yellow } from '@std/fmt/colors'
+import { Select } from '@cliffy/prompt'
+import { brightBlack, rgb24 } from '@std/fmt/colors'
 import { hyperlink } from './fmt.ts'
 import './polyfills.ts'
+import { cursorUp, eraseLines } from '@cliffy/ansi/ansi-escapes'
 
 function flattenKeys(obj: object, bottomProps: string[], acc: string[] = []): { key: string[]; value: unknown }[] {
 	return Object.entries(obj).flatMap(([key, value]) => {
@@ -18,11 +20,11 @@ function flattenKeys(obj: object, bottomProps: string[], acc: string[] = []): { 
 }
 
 const bottomProps = ['__compat']
-const excludeFromKeys = ['javascript', 'api', 'builtins']
+const excludeFromKeys = ['javascript', 'api', 'builtins', 'grammar']
 
 const bcdSearchable = flattenKeys(bcd, bottomProps).map(({ key, value }) => ({
 	key,
-	keywords: key.filter((k) => !excludeFromKeys.includes(k)).join(' '),
+	keywords: key.filter((k) => !excludeFromKeys.includes(k)).join(' ').replaceAll(/[^\p{L}\p{M}\p{N}]+/gu, ' ').trim(),
 	data: value as CompatStatement,
 }))
 
@@ -71,51 +73,110 @@ export const cli = new Command()
 		`pat set union ${brightBlack('# get results for keywords "set union", e.g. `Set.prototype.union`')}`,
 	)
 	.arguments('<keyword:string> [...keywords:string]')
-	.action((_options, ...keywords) => {
-		const results = fuse.search(keywords.join(' '))
+	.action(handler)
 
-		if (!results.length) {
-			console.error('No results found.')
-			return
-		}
+type Result = FuseResult<{
+	key: string[]
+	keywords: string
+	data: CompatStatement
+}>
 
-		const { item } = results[0]
-		const { key, data } = item
+function getResultTable(result: Result) {
+	const { item } = result
+	const { key, data } = item
 
-		console.info()
+	const prelude = [
+		'',
+		data.mdn_url ? `${item.keywords} [${hyperlink('MDN', data.mdn_url)}]` : item.keywords,
+		brightBlack(key.join('.')),
+		'',
+	].join('\n')
 
-		console.info(data.mdn_url ? `${item.keywords} [${hyperlink('MDN', data.mdn_url)}]` : item.keywords)
-		console.info(brightBlack(key.join('.')))
+	const supportInfo = Object.entries(data.support).map(([k, v]) => {
+		if (excludeFromSupportInfo.includes(k)) return null
+		return [k, Array.isArray(v) ? v[0] : v] as const
+	}).filter((x) => x != null)
+		.map(([k, v]) => {
+			return { browser: k, ...v }
+		})
 
-		const supportInfo = Object.entries(data.support).map(([k, v]) => {
-			if (excludeFromSupportInfo.includes(k)) return null
-			return [k, Array.isArray(v) ? v[0] : v] as const
-		}).filter((x) => x != null)
-			.map(([k, v]) => {
-				return { browser: k, ...v }
-			})
+	return prelude + table(
+		['Browser', 'Version Added', 'Bug Tracker'],
+		supportInfo.map((x) => {
+			const { version_added } = x
+			const v = version_added === true
+				? rgb24('[unknown]', RECENT)
+				: version_added || rgb24('[none]', UNSUPPORTED)
 
-		console.info(
-			table(
-				['Browser', 'Version Added', 'Bug Tracker'],
-				supportInfo.map((x) => {
-					const { version_added } = x
-					const v = version_added === true
-						? rgb24('[unknown]', RECENT)
-						: version_added || rgb24('[none]', UNSUPPORTED)
+			const { name: browserName, versionInfo } = getBrowserInfo(x.browser, v)
 
-					const { name: browserName, versionInfo } = getBrowserInfo(x.browser, v)
+			const versionInfoFormatted = versionInfo?.release_date ? formatVersionInfo(v, versionInfo.release_date!) : v
 
-					const versionInfoFormatted = versionInfo?.release_date
-						? formatVersionInfo(v, versionInfo.release_date!)
-						: v
+			const bugTrackerUrl = getBugTrackerUrl(firstOrOnly(x.impl_url, null))
 
-					const rowData = [browserName, versionInfoFormatted, firstOrOnly(x.impl_url, '')]
-					return rowData
-				}),
-			),
-		)
-	})
+			const rowData = [browserName, versionInfoFormatted, bugTrackerUrl]
+			return rowData
+		}),
+	)
+}
+
+async function handler(_options: void, ...keywords: string[]) {
+	const results = fuse.search(keywords.join(' '))
+
+	if (!results.length) {
+		console.error('No results found.')
+		return
+	}
+
+	let table = ''
+	let resultIdx = 0
+
+	function drawTable() {
+		table = getResultTable(results[resultIdx])
+		console.info(table)
+	}
+
+	drawTable()
+
+	const maxRows = 5
+
+	while (true) {
+		resultIdx = await Select.prompt({
+			message: 'View a different result',
+			options: results.slice(0, 100).map((x, i) => {
+				const name = `${i + 1}. ${i === resultIdx ? brightBlack('[current] ') : ''}${x.item.keywords} ${
+					brightBlack(x.item.key.join('.'))
+				}`
+
+				return {
+					name,
+					value: i,
+					disabled: i === resultIdx,
+				}
+			}),
+			maxRows,
+		})
+
+		const linesToErase = [...table.matchAll(/\n/g)].length + maxRows - 2
+		console.info(eraseLines(linesToErase))
+		console.info(cursorUp(2))
+
+		drawTable()
+	}
+}
+
+function truncate(str: string, maxLength: number) {
+	const chars = [...str]
+	return chars.length <= maxLength ? str : chars.slice(0, maxLength).join('') + '…'
+}
+
+function getBugTrackerUrl(url: string | null) {
+	if (!url) return ''
+
+	const { host, pathname } = new URL(url)
+
+	return hyperlink(truncate(host + pathname, 80), url)
+}
 
 function formatVersionInfo(v: string, release_date: string) {
 	const { ago, rgb } = getAgo(release_date)
